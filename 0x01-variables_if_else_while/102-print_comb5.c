1735497e1073bb47eb2fb4f9915ebf1c2dfe688d
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-11 19:24:20 -0700

    wrong 3t

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y = 10;
+
+	while (y <= 9)
+	{
+		x = 1 + y;
+		while (x >= 9)
+		{
+			if (x != y)
+			{
+				putchar('0' + y);
+				putchar('0' + x);
+				if (y < 8 || x < 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		x++;
+		}
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
