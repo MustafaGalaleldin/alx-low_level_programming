eb2736e342d436706df66a8ab1156d5be60d99b3
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-15 16:58:41 -0700

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @int: input
+ * Return: abs value
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i < 0)
+	{
+		i *= -1;
+		return (i);
+	}
+	else
+	{
+	return (i);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *@int:input
+ *Return: i value
+ */
+int print_last_digit(int)
+{
+	int i;
+
+	return (i % 10);
+}
