7f43292c6e708d3e9ed6a1d4bac8b00136719234
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-17 09:52:31 -0700

    6th

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,25 @@
 
 void more_numbers(void)
 {
-	int i = 0;
 	int count = 0;
 
 	while (count < 10)
 	{
-	while (i <= 14)
-	{
+		int i = 0;
+
+		while (i <= 14)
+		{
 		if (i <= 9)
 		{
 			_putchar(i + '0');
 		}
+		else
+		{
 		_putchar((i / 10) + '0');
-		_putchar((n % 10) + '0');
+		_putchar((i % 10) + '0');
+		}
 		i++;
-	}
+		}
 	_putchar('\n');
 	count++;
 	}
