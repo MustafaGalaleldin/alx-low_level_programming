77afc9bb80fc9002c42277e83e1c14024f0832ab
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-16 19:54:56 -0700

    hh

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,26 +6,17 @@
  */
 int main(void)
 {
-	int sum;
-	int sum3, sum5 = 0;
-	int n = 0;
-	int m = 0;
-	int mul3 = 0;
-	int mul5 = 0;
+	int sum = 0;
+	int num = 0;
 
-	while (mul3 < 1024)
+	while (num < 1024)
 	{
-		sum3 += mul3;
-		mul3 = n * 3;
-		n++;
+		if ((num %3 == 0) || (num % 5 ==0))
+		{
+			sum += num;
+		}
+		num++;
 	}
-	while (mul5 < 1024)
-	{
-		sum5 += mul5;
-		mul5 = m * 5;
-		m++;
-	}
-	sum = sum3 + sum5;
 	printf("%d\n", sum);
 	return (0);
 }
