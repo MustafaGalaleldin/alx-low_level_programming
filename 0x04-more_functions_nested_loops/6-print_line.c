9337184c68cea981570edd895778af65b4707589
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-17 11:18:19 -0700

    7th

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - pr1
+ * @n: input
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+	else
+	{
+	_putchar('\n');
+	}
+}
