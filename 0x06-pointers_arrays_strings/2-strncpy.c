3bcdee050d66ab4731aa71f77c82597e9538ce12
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-24 03:30:20 -0700

    solved

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - that copies a string by bytes
+ * @dest: string
+ * @src: source string
+ * @n: bytes
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	}
+	return (dest);
+}
