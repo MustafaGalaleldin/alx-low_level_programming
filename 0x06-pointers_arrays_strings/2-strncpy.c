92ad37a270d56f2addd3d61e138420fadb5757e6
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-24 03:54:04 -0700

    kk

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
+		if (src[i] == '\0')
+		{
+			dest[i] = '\0';
+		}
+		else
+		{
 	dest[i] = src[i];
+		}
 	}
 	return (dest);
 }
