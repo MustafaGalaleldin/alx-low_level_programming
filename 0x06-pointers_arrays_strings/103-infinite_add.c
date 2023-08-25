e78b548b645073ee3207e2e521693c3d8aff25df
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-08-25 13:14:12 -0700

    m

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+		i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+	temp = *(n + j);
+	*(n + j) = *(n + i);
+	*(n + i) = temp;
+	}
+}
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -20,7 +47,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	j--;
 	if (j >= size_r || i >= size_r)
 	return (0);
-	while (j >= 0  overflow == 1)
+	while (j >= 0 ||  overflow == 1)
 	{
 	if (i < 0)
 		val1 = 0;
@@ -31,7 +58,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		val2 = *(n2 + j) - '0';
 	temp_tot = val1 + val2 + overflow;
-	if (temp_tot >= 10)
+	if (temp_tot > 10)
 		overflow = 1;
 	else
 		overflow = 0;
