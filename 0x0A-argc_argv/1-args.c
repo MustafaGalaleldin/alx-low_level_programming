e76162bc569cca7dc157abfe34aae81ed54da1ac
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-01 14:58:45 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: c
+ * @argv: v
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
