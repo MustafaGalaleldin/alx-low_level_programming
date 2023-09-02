eead7edca0b4790147da8d1f53b55416ce0a4b6a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-01 17:05:29 -0700

    k

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 /**
- * main - entry
+ * ch_num - check
+ * @str: str
+ *
+ * Return: 0
+ */
+int ch_num(char *str)
+{
+	unsigned int c;
+
+	c = 0;
+	while (c < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+
+		c++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
  * @argc: c
  * @argv: v
+ *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
-{
-	int sum = 0;
-	char *z;
-	int x = argc;
 
-	while (--x)
+{
+
+	int c;
+	int z;
+	int sum = 0;
+
+	c = 1;
+	while (c < argc)
 	{
-		for (z = argv[x]; *z; z++)
+		if (ch_num(argv[count]))
+
 		{
-			if (*z < '0' || *z > '9')
-			{
+			z = atoi(argv[count]);
+			sum += z;
+		}
+
+		else
+		{
 			printf("Error\n");
 			return (1);
-			}
-		sum += atoi(argv[x]);
 		}
+
+		c++;
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
+
