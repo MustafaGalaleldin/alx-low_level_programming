3a5fd5f37729ee41720849ab219f4c2ded8f86ce
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-04 06:10:20 -0700

    f

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *ptr;
-
-	if (size == 0)
+	
+	ptr = malloc(size * sizeof(char));
+	if (size == 0 || ptr == 0)
 	{
 		return ('\0');
 	}
-	ptr = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
