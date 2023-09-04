00f33b486481be24b6323387e4418655b752fb23
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-04 06:04:07 -0700

    1st bug fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
-	ptr = malloc(size * sizeof(char));
+	ptr = (char *)malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
