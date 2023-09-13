10297ba4dc3c86e818a9e43bc0f9e894a8b3fc52
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-13 10:59:37 -0700

    0 task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - that prints a name
+ * @name: to print
+ * @f: func
+ * Return: non
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
