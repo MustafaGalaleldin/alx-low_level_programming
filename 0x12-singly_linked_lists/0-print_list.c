cf342b682c7682334ba07b1336426cd12a2fbee4
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-21 16:59:09 -0700

    T0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "list.h"
+
+/**
+ * _strlen - l
+ * @s: input
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: str
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
