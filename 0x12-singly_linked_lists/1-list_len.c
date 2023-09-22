05b1a41d2022bc2873d83c0d18905cc3723731f9
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-09-21 17:15:28 -0700

    T1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer
+ * Return: num of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t t = 0;
+
+	while (h)
+	{
+		h->next;
+		t++;
+	}
+	return (t);
+}
