b750122f0e1385088b7f2db6c4102ccae8ef4aa6
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-10-25 12:26:12 -0700

    bug to fix in prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@
 
 int hprime(int x, int n)
 {
-	if(x < n)
+	if (x < n)
 	{
 		if (!(n % x))
 			return (0);
@@ -34,7 +34,7 @@ int is_prime_number(int n)
 {
 	if (n < 0)
 		n = -n;
-	if (n == 1)
+	if (n == 1 || n == 0)
 		return (0);
 	if (n == 2)
 		return (1);
