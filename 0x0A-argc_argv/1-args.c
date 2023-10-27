41429ee71fba5b288b72153f87e8dab6c1245330
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-10-27 10:13:48 -0700

    args counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @args: args counter
+ * @argv: args vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
