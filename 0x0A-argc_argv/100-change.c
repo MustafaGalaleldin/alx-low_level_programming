d0ff07f43b7849a88803e98cbc67b64fe3e40009
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-10-28 07:42:38 -0700

    advanced done

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: args counter
+ * @argv: args vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int x;
+	int r = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+x = atoi(argv[1]);
+	if (x < 0)
+		printf("0\n");
+	while (x)
+	{
+		if (x >= 25)
+		{
+			x -= 25;
+			r++;
+			continue; }
+		if (x >= 10)
+		{
+			x -= 10;
+			r++;
+			continue;
+		}
+		if (x >= 5)
+		{	x -= 5;
+			r++;
+			continue; }
+		if (x >= 2)
+		{	x -= 2;
+			r++;
+			continue; }
+		if (x >= 1)
+		{	x -= 1;
+			r++;
+			continue; }
+	}
+	printf("%d\n", r);
+	return (0);
+}
