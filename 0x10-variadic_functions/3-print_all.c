558e36b9604631dd8d4f617b19f88dfdbb55a3b0
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-09 13:04:37 -0800

    test

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,38 +34,37 @@ void print_all(const char * const format, ...)
 	va_list ar;
 	int len, i = 0;
 
-	if (format)
+	if (!format)
 	{
-		len = _strlen(format);
-		va_start(ar, format);
-		while (i < len)
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(ar, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(ar, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(ar, double));
-					break;
-				case 's':
-					printf("%s", va_arg(ar, char *));
-					break;
-				default:
-					i++;
-					continue;
-			}
-			if (i + 1 != len)
-				printf(", ");
-			else
-				printf("\n");
-			i++;
-		}
-		va_end(ar);
+		printf("\n");
 	}
+	len = _strlen(format);
+	va_start(ar, format);
+	while (i < len)
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ar, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ar, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ar, double));
+				break;
+			case 's':
+				printf("%s", va_arg(ar, char *));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i + 1 != len)
+			printf(", ");
 		else
 			printf("\n");
+		i++;
+	}
+		va_end(ar);
 }
