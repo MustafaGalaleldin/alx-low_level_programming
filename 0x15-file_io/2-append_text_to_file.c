a15978c1e7de7c6f061072f97b184b5a4cbb293a
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-27 11:11:27 -0800

    ll

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,4 +10,17 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd;
+	ssize_t ret = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (_strlen(text_content))
+		ret = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	return (ret == _strlen(text_content) ? 1 : -1);
+}
 
