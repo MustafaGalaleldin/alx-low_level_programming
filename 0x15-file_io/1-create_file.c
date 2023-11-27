7cb25fc9b7382f1fb163849e3b082140c23a9aca
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-27 10:56:50 -0800

    ww

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (_strlen(text_content))
-		ret = write(fd, text_content, strlen(text_content));
+		ret = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	return (ret == _strlen(text_content) ? 1 : -1);
 }
