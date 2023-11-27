c231189bad928f8de2d276cc51677c008573e394
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-27 10:55:47 -0800

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+#include "main.h"
+/**
+ * _strlen - l
+ * @s: input
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (!s)
+		return (0);
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
 /**
  * create_file - creates a file
  *
@@ -18,9 +38,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDONLY, O_CREAT, O_TRUNC, O600);
 	if (fd == -1)
 		return (-1);
-	if (strlen(text_content))
+	if (_strlen(text_content))
 		ret = write(fd, text_content, strlen(text_content));
 	close(fd);
-	return (ret == strlen(text_content) ? 1 : -1);
+	return (ret == _strlen(text_content) ? 1 : -1);
 }
-
