19148144eabcd362140cf0c2a122c9dbadb3c891
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2023-11-27 11:23:54 -0800

    tt

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - l
+ * @s: input
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (!s)
+		return (0);
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+/**
  * append_text_to_file - appends text at the end of a file.
  *
  * @filename: name
