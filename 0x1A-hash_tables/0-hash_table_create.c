68b77cac1cc6f1c5771eaf417406bee688f38052
MustafaGalaleldin <mustafagalaleldin@gmail.com>
2024-04-12 02:40:46 +0200

    1st fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,15 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table;
-        unsigned long int i;
+	hash_table_t *table;
 
-        table = malloc(sizeof(table));
-        if (!table)
-                return (NULL);
-        table->size = size;
-        table->array = malloc(size * sizeof(hash_node_t *));
-        if (!table->array)
-                return (NULL);
-        for (i = 0; i < size; i++) {
-                table->array[i] = NULL;
-        }
-        return (table);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	/* calloc to set array positions to 0 */
+	if (!table->array)
+		return (NULL);
+	return (table);
 }
